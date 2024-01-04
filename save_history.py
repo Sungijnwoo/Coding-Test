4b4a3e0cf171eead0c55a7c9c45bd11a321ced93
Sungijnwoo <tjdwnsdud3@naver.com>
2024-01-04 15:33:57 +0900

    1000/0.3

--- save_history.py
+++ save_history.py
@@ -3,30 +3,24 @@ import gspread
 from datetime import datetime
 
 def main(args):
-    try:
-        commit_message: str = args.commit_message
-        problem_number, duration = commit_message.split("/")
-        problem_number = int(problem_number)
-        commit_number = args.commit_number        
-        user_name = args.user_name
-        site_address = f"https://www.acmicpc.net/problem/{problem_number}"
-        commit_address = f"https://github.com/Sungijnwoo/Coding-Test/commit/{commit_number}"
-        duration = float(duration)
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        gc = gspread.service_account(filename='google-services.json') # 수정됨
-        sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1k4sifuVCdUsaR_apfofK0gXaFud0UwjMoL-3xj1NwJE/edit#gid=0')
-        worksheet = sheet.worksheet("history") # 이름으로 워크시트 선택
-        start_row = len(worksheet.col_values(1)) + 1
-        print(user_name, site_address, commit_address, duration, current_time)
-        worksheet.update_cell(start_row, 1, user_name)
-        worksheet.update_cell(start_row, 2, site_address)
-        worksheet.update_cell(start_row, 3, commit_address)
-        worksheet.update_cell(start_row, 4, f"{duration}")
-        worksheet.update_cell(start_row, 5, current_time)
-        
-    except Exception as e:
-        print(e)
-        print("commit message error")
+    commit_message: str = args.commit_message
+    problem_number, duration = commit_message.split("/")
+    problem_number = int(problem_number)
+    commit_number = args.commit_number        
+    user_name = args.user_name
+    site_address = f"https://www.acmicpc.net/problem/{problem_number}"
+    commit_address = f"https://github.com/Sungijnwoo/Coding-Test/commit/{commit_number}"
+    duration = float(duration)
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    gc = gspread.service_account(filename='google-services.json') # 수정됨
+    sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1k4sifuVCdUsaR_apfofK0gXaFud0UwjMoL-3xj1NwJE/edit#gid=0')
+    worksheet = sheet.worksheet("history") # 이름으로 워크시트 선택
+    start_row = len(worksheet.col_values(1)) + 1
+    worksheet.update_cell(start_row, 1, user_name)
+    worksheet.update_cell(start_row, 2, site_address)
+    worksheet.update_cell(start_row, 3, commit_address)
+    worksheet.update_cell(start_row, 4, f"{duration}")
+    worksheet.update_cell(start_row, 5, current_time)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
