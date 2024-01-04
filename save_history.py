e497534d26c65bae9553dd3580081f1843618aab
Sungijnwoo <tjdwnsdud3@naver.com>
2024-01-04 15:29:58 +0900

    1000/0.3

--- save_history.py
+++ save_history.py
@@ -13,18 +13,18 @@ def main(args):
         commit_address = f"https://github.com/Sungijnwoo/Coding-Test/commit/{commit_number}"
         duration = float(duration)
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        gc = gspread.service_account(filename='code-test-410204-08bdafa0ea37.json') # 수정됨
+        gc = gspread.service_account(filename='google-services.json') # 수정됨
         sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1k4sifuVCdUsaR_apfofK0gXaFud0UwjMoL-3xj1NwJE/edit#gid=0')
         worksheet = sheet.worksheet("history") # 이름으로 워크시트 선택
         start_row = len(worksheet.col_values(1)) + 1
-
         worksheet.update_cell(start_row, 1, user_name)
         worksheet.update_cell(start_row, 2, site_address)
         worksheet.update_cell(start_row, 3, commit_address)
         worksheet.update_cell(start_row, 4, duration)
         worksheet.update_cell(start_row, 5, current_time)
         
-    except:
+    except Exception as e:
+        print(e)
         print("commit message error")
 
 if __name__ == "__main__":
