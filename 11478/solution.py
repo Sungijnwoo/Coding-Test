5772f882bb64d6c943c769178cb1189f1bbb94c0
Sungijnwoo <tjdwnsdud3@naver.com>
2024-01-11 09:06:58 +0900

    11478/0.2

---
+++ 11478/solution.py
@@ -0,0 +1,16 @@
+def main():
+    S = input()
+    subStringList = []
+
+    for i in range(1, len(S)+1):
+        for j in range(len(S) - i + 1):
+            subString = S[j: j+i]
+            subStringList.append(subString)
+    subStringSet = set(subStringList)
+    print(len(subStringSet))
+    
+
+if __name__ == "__main__":
+    main()
+
+
